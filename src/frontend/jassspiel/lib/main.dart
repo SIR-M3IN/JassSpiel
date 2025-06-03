61e56691d7df831f6f3ecb0848444e7d73720cc1
Merlin <merlin.dercoole@outlook.com>
2025-06-03 12:10:34 +0200

    feat: add logger utility and integrate logging in main.dart

--- src/frontend/jassspiel/lib/main.dart
+++ src/frontend/jassspiel/lib/main.dart
@@ -5,9 +5,17 @@ import 'package:flutter/services.dart';
 import 'pages/game.dart';
 import 'pages/users_page.dart';
 import 'pages/start.dart';
+import 'package:logger/logger.dart'; 
+import 'logger.util.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Logger.level = Level.debug;
+  final log = getLogger();
+  log.d('Debug-Nachricht aus der main-Methode.');
+  log.i('Info-Nachricht aus der main-Methode.');
+  log.e('Error-Nachricht aus der main-Methode.');
   await Supabase.initialize(
     url: 'https://wzhaxvxfhdcrpyiswybf.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6aGF4dnhmaGRjcnB5aXN3eWJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NDA1MTEsImV4cCI6MjA2MjAxNjUxMX0.yzYZ4jHfAlq2CgpkN_oAue71LLNzAYzP0ABSj1YbFNs',
@@ -36,6 +44,7 @@ class CardGameApp extends StatelessWidget {
 
     onGenerateRoute: (settings) {
       //KI: Fix das 
+
     if (settings.name == '/init') {
       final args = settings.arguments;
       if (args is Map<String, dynamic> && args['gid'] is String && args['uid'] is String) {
